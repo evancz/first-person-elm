7d6fabb87045e7514571cf1fcf4c8305a539fb92
Evan Czaplicki <info@elm-lang.org>
2014-06-01 07:53:12 -0500

    Get rid of cost of rendering text.

--- src/Display.elm
+++ src/Display.elm
@@ -19,11 +19,8 @@ scene : (Int,Int) -> Bool -> Response Texture -> Model.Person -> Element
 scene (w,h) isLocked texture person =
     layers [ color (rgb 135 206 235) (spacer w h)
            , webgl (w,h) (entities texture (view (w,h) person))
-           , container w 140 (midLeftAt (absolute 40) (relative 0.5)) . plainText <|
-               "This uses stuff that is only available in Chrome and Firefox!\n\nWASD keys to move, space bar to jump.\n\n" ++
-               if isLocked
-                  then "Press <escape> to exit full screen."
-                  else "Click to go full screen and move your head with the mouse."
+           , container w 140 (midLeftAt (absolute 40) (relative 0.5))
+                 (if isLocked then exitMsg else enterMsg)
            ]
 
 entities : Response Texture -> Mat4 -> [Entity]
@@ -37,3 +34,15 @@ entities response view =
                    _ -> []
     in  
         ground view :: crates
+
+enterMsg : Element
+enterMsg = message "Click to go full screen and move your head with the mouse."
+
+exitMsg : Element
+exitMsg = message "Press <escape> to exit full screen."
+
+message : String -> Element
+message msg =
+    plainText <|
+    "This uses stuff that is only available in Chrome and Firefox!\n" ++
+    "\nWASD keys to move, space bar to jump.\n\n" ++ msg
